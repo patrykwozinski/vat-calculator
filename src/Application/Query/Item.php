1aab54c5699ec028d7e9a4999b7f652d053ff3ba
Patryk Woziński <patryk.wozinski@gmail.com>
2018-12-13 22:42:52 +0100

    Projection in Item\ Application layer

--- src/Application/Query/Item.php
+++ src/Application/Query/Item.php
@@ -10,16 +10,19 @@ declare(strict_types=1);
 namespace Freeq\VatCalculator\Application\Query;
 
 
+use Freeq\VatCalculator\Domain\Shared\ReadModelProjection;
+
 final class Item
 {
+    /** @var ReadModelProjection */
     private $model;
 
-    public function __construct($model)
+    public function __construct(ReadModelProjection $model)
     {
         $this->model = $model;
     }
 
-    public function model()
+    public function model(): ReadModelProjection
     {
         return $this->model;
     }
--- src/Domain/Price/Projection/PriceView.php
+++ src/Domain/Price/Projection/PriceView.php
@@ -10,6 +10,16 @@ declare(strict_types=1);
 namespace Freeq\VatCalculator\Domain\Price\Projection;
 
 
-final class PriceView
+use Freeq\VatCalculator\Domain\Shared\ReadModelProjection;
+
+final class PriceView implements ReadModelProjection
 {
+    public function __construct()
+    {
+    }
+
+    public function serialize(): array
+    {
+        // TODO: Implement serialize() method.
+    }
 }
