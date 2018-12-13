a73c24678ba95a0456c893e06347040b209307e7
Patryk Woziński <patryk.wozinski@gmail.com>
2018-12-13 01:11:50 +0100

    Basic of tests

---
+++ tests/Application/ApplicationTestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 13/12/2018
+ * Time: 01:09
+ */
+declare(strict_types=1);
+
+namespace Freeq\VatCalculator\Tests\Application;
+
+
+use PHPUnit\Framework\TestCase;
+
+abstract class ApplicationTestCase extends TestCase
+{
+}
---
+++ tests/Domain/DomainTestCase.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: freeq
+ * Date: 13/12/2018
+ * Time: 01:08
+ */
+declare(strict_types=1);
+
+namespace Freeq\VatCalculator\Tests\Domain;
+
+
+use PHPUnit\Framework\TestCase;
+
+abstract class DomainTestCase extends TestCase
+{
+}
